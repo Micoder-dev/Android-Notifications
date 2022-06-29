8cbbe72f1cfa9030436ca2300d1138c052329fde
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-29 22:30:42 +0530

    push notification from firebase works fine

---
+++ app/src/main/java/com/micoder/androidnotifications/NotificationHelper.java
@@ -0,0 +1,25 @@
+package com.micoder.androidnotifications;
+
+import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationHelper {
+
+
+    public static void displayNotification(Context context, String title, String body) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(context);
+        mNotificationMgr.notify(1, mBuilder.build());
+
+    }
+
+}
